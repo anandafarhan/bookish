f1224ec860001f31aaeef09cce2b80cf5b534bb0
Ananda Farhan <ananda.farhan@meteor.id>
2023-10-30 18:14:31 +0700

    rename app & bundle id

--- ios/BookishTests/BookishTests.m
+++ ios/BookishTests/BookishTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MeteorBoilerplateTests : XCTestCase
+@interface BookishTests : XCTestCase
 
 @end
 
-@implementation MeteorBoilerplateTests
+@implementation BookishTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
